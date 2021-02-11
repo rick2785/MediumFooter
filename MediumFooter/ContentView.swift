47c511829be2c0b0385cf427682e6401f4f4361a
rick2785 <rjhrabowskie@gmail.com>
2021-02-11 10:36:58 -0600

    Initial Commit

---
+++ MediumFooter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MediumFooter
+//
+//  Created by RJ Hrabowskie on 2/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MediumFooter/MediumFooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediumFooterApp.swift
+//  MediumFooter
+//
+//  Created by RJ Hrabowskie on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MediumFooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
